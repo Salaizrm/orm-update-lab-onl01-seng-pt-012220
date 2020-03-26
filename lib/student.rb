b09211564d8b724808419e5a16b782ecf3c5bb97
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-26 17:21:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,9 +35,8 @@ class Student
     VALUES (?,?)
     SQL
 
-    DB[:conn].execute(sql. self.name, self.grade).map do |row|
-      self.new_from_db(row)
-    end
+    DB[:conn].execute(sql. self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
 
