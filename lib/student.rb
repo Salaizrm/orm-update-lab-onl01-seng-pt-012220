69075bf5f6605fb929bc61af673465f78d651e4f
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-26 17:48:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,12 +68,15 @@ class Student
     self.new(id, name, grade)
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT name
+    FROM students
+    WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name)
   end
-
-
-
-
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
